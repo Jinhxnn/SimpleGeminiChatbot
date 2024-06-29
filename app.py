9fb7f0e1d24134f449e5e728a85443b6183cc50b
jinhznn <133880039+Jinhxnn@users.noreply.github.com>
2024-06-29 17:28:58 +0800

    changed the code

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ if prompt := st.chat_input("Say something:"):
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        st.write_stream(response_generator(chat_session.last.text))
+        st.write_stream(chat_session.last.text)
 
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": chat_session.last.text})
